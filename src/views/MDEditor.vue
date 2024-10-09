5aa30c97fa6e423c406e18d610a31f48c4695c77
李文豪 <1457434343@qq.com>
2024-10-09 18:01:48 +0800

    feat: update

--- src/views/MDEditor.vue
+++ src/views/MDEditor.vue
@@ -1,13 +1,18 @@
 <template>
     <div>
         <h1 class="text-2xl font-bold mb-4">Markdown Editor</h1>
-        <v-md-editor v-model="text" height="800px"></v-md-editor>
-        <h1 class="text-2xl font-bold mb-4 mt-4">Preview</h1>
-        <v-md-editor :model-value="text" height="800px" mode="preview"></v-md-editor>
+        <v-md-editor v-model="text" height="800px" @save="save"></v-md-editor>
+        <div class="lg:hidden">
+            <h1 class="text-2xl font-bold mb-4 mt-4">Preview</h1>
+            <v-md-editor :model-value="text" mode="preview"></v-md-editor>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 const text = ref('')
+const save = (text: string, html: string) => {
+    console.log(html)
+}
 </script>
