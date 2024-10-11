0de711be176409219928675381588c5b0a4eafa0
kamisama <1457434343@qq.com>
2024-10-11 21:20:13 +0800

    Updates

---
+++ assets/AboutView-Bqq25uSp.js
@@ -0,0 +1,1 @@
+import{_ as o,b as s,a as t,o as a}from"./index-D97R-yDI.js";const n={},r={class:"about"};function c(_,e){return a(),s("div",r,e[0]||(e[0]=[t("h1",null,"This is an about page",-1)]))}const l=o(n,[["render",c]]);export{l as default};
