ae43b17c905ee708098df9feb8ce592776a95801
kamisama <1457434343@qq.com>
2024-10-12 03:01:55 +0800

    Updates

---
+++ assets/blog-DuDT1sDk.js
@@ -0,0 +1,1 @@
+import{i as e}from"./index-D5-_EQyO.js";const o=t=>e.post("/api/blogs/add",t),a=()=>e.get("/api/blogs/list"),r=t=>e.post(`/api/blogs/delete/${t}`,{id:t});export{o as c,r as d,a as g};
