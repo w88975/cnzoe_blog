6cc91dfb6e7042abdc5f0a833d65b52a952e5cda
kamisama <1457434343@qq.com>
2024-10-14 07:20:42 +0800

    Updates

--- assets/BoxView-Bdj1oUb4.js
+++
@@ -1,1 +0,0 @@
-import{m as o,o as s,a as t,P as r}from"./index-CjaOFfz9.js";const c={},n={class:"bg-white rounded-md p-4 mb-5"};function a(e,d){return s(),t("div",n,[r(e.$slots,"default")])}const i=o(c,[["render",a]]);export{i as B};
