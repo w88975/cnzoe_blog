5bd80b1cb2490ba3589d7c3a8176c1581793b08d
kamisama <1457434343@qq.com>
2024-10-19 04:47:38 +0800

    Updates

--- assets/blog-rlQGIepN.js
+++
@@ -1,1 +0,0 @@
-import{i as e}from"./index-CDBvrqn-.js";const o=t=>e.post("/api/blogs/add",t),a=()=>e.get("/api/blogs/list"),r=t=>e.post(`/api/blogs/delete/${t}`,{id:t});export{o as c,r as d,a as g};
