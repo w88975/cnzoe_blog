620a52b230e8e43102675b4c26b465fc266ee857
kamisama <1457434343@qq.com>
2024-10-21 02:53:39 +0800

    Updates

--- assets/blog-D8wFkUsT.js
+++
@@ -1,1 +0,0 @@
-import{x as e}from"./index-5aWETVbQ.js";const o=t=>e.post("/api/blogs/add",t),a=()=>e.get("/api/blogs/list"),r=t=>e.post(`/api/blogs/delete/${t}`,{id:t});export{o as c,r as d,a as g};
