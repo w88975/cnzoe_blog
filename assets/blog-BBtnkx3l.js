d2bff30e647e0e5626960b0f976e34056596d6e0
kamisama <1457434343@qq.com>
2024-10-20 23:58:38 +0800

    Updates

---
+++ assets/blog-BBtnkx3l.js
@@ -0,0 +1,1 @@
+import{C as e}from"./index-BekGhLC_.js";const o=t=>e.post("/api/blogs/add",t),a=()=>e.get("/api/blogs/list"),r=t=>e.post(`/api/blogs/delete/${t}`,{id:t});export{o as c,r as d,a as g};
