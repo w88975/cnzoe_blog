4eaf5843eafcc683c4ba820d54aa1aabe979623a
李文豪 <1457434343@qq.com>
2024-10-21 15:54:42 +0800

    Updates

---
+++ assets/blog-a5BOHqIO.js
@@ -0,0 +1,1 @@
+import{q as e}from"./index-CuLL_66A.js";const o=t=>e.post("/api/blogs/add",t),a=()=>e.get("/api/blogs/list"),r=t=>e.post(`/api/blogs/delete/${t}`,{id:t});export{o as c,r as d,a as g};
